b8416d5d926090b654a580e9075ed8010b564992
nrontsis <nrontsis@gmail.com>
2025-06-06 14:49:52 +0000

    Add script to build manylinux wheels

---
+++ build_wheels.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+docker run --rm -v "$(pwd)":/io quay.io/pypa/manylinux_2_34_x86_64 \
+    /bin/bash -c '
+    set -e
+    for PYBIN in /opt/python/cp{311,312,313}*/bin; do
+        "${PYBIN}/pip" wheel /io -w /tmp/wheels
+    done
+    auditwheel repair /tmp/wheels/*.whl -w /io/wheelhouse
+    chown -R '"$(id -u):$(id -g)"' /io/wheelhouse
+'
